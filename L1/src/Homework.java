c2662a0c1005ae2be0131570ac6d0f0cb054cb16
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-02-27 16:39:47 +0200

    Homework without bonus added

--- L1/src/Homework.java
+++ L1/src/Homework.java
@@ -7,76 +7,88 @@ public class Homework {
 //		return x == true? 1 : 0;
 //	}
 
-	private static boolean areNeighbours(String s1, String s2)
+	//Functie ce verifica daca doua cuvinte sunt vecine
+	private static boolean areNeighbours(StringBuilder s1, StringBuilder s2)
 	{
 		for(int i=0; i < s1.length(); i++)
 			for(int j=0; j < s2.length(); j++)
-				if(s1.charAt(i) == s2.charAt(j))
+				if(s1.charAt(i) == s2.charAt(j))//daca gasim un caracter comun
 					return true;
 		return false;
 	}
 	
 	public static void main(String[] args) {
 		
+		//preiau in startTime timpul la care incepe programul
 		int startTime = (int) System.nanoTime();
-		//1
-		int n = Integer.parseInt(args[0]);
-		int p = Integer.parseInt(args[1]);
-		char[] c = new char[args.length - 2];
+		int n = Integer.parseInt(args[0]);//luam primul parametru ce este chiar n
+		int p = Integer.parseInt(args[1]);//luam al doilea parametru, si anume p
+		char[] c = new char[args.length - 2];//in c se va afla alfabetul
 		
 		int index = 0;
-		for(int i = 2; i < args.length; i++)
-			c[index++] = args[i].charAt(0);
+		//pornim de la 2 prin argumente deoarece in 0 avem n si in 1 pe p, alfabetul incepand de la 2 pana la args.length
+		for(int i = 2; i < args.length; i++) 
+			c[index++] = args[i].charAt(0);//am pus charAt(0) ca sa-l vada ca si caracter.
 		
-		//2
-		String[] words = new String[n];
+		//acum in c avem alfabetul
+		
+		StringBuilder[] words = new StringBuilder[n];//aici vom avea cuvintele generate aleator
 		for(int i = 0; i < n; i++)
 		{
+			words[i] = new StringBuilder();
 			Random r = new Random();
 			int low = 0;
-			int high = c.length - 1;
-			int result = r.nextInt(high-low) + low;
-			words[i] = c[result] + "";
-			for(int j = 0; j < p; j++)
+			int high = c.length;
+			int result = r.nextInt(high-low) + low;//in result avem un numar random din intervalul [low,high].
+			words[i].append(c[result]);//punem un caracter
+			for(int j = 1; j < p; j++)//punem urmatoarele p-1 caractere
 			{
 				r = new Random();
 				low = 0;
 				high = c.length - 1;
 				result = r.nextInt(high-low) + low;
-				words[i] = words[i] + c[result];
+				words[i].append(c[result]);
 			}
-			System.out.println(words[i]);
 		}
 		
-		//3
 		boolean[][] adj = new boolean[n][n];
-		for(int i = 0; i < n; i++)
-			for(int j = 0; j < n; j++)
-					adj[i][j] = areNeighbours(words[i],words[j]);
+		for(int i=0;i<words.length;i++)
+			for(int j=0;j<words.length;j++)
+			{
+				if(areNeighbours(words[i],words[j]) == true)
+				{
+					adj[i][j] = true;
+					//System.out.print("\nVecini " + words[i] + " cu " + words[j]);
+				}
+				else
+					adj[i][j] = false;
+			}
+		System.out.println();
+		Word[] wordDB = new Word[n];
+		for(int i = 0; i < n ; i++)
+		{
+			wordDB[i] = new Word();
+			wordDB[i].setNeighbourhood(i, words, adj, n);
+		}
+		for(int i = 0; i < n ; i++)
+		{
+			wordDB[i].printNeighbours(i,words);
+			System.out.println();
+		}
 		
-//		for(int i=0;i<n;i++)
+		
+		
+//		for(int i=0;i<words.length;i++)
 //		{
-//			System.out.print(words[i]+": ");
-//			for(int j=0;j<n;j++)
+//			for(int j=0;j<words.length;j++)
 //				System.out.print(adj[i][j]+" ");
 //			System.out.println();
 //		}
-//			
-		
-		Word word[] = new Word[20];
-		for(int i = 0; i < n; i++)
-		{
-			word[i] = new Word();
-			word[i].setNeighbourhood(i, words, adj, n);
-		}
 		
 		
-		for(int i = 0; i < n; i++)
-		{   
-			word[i].printNeighbours(index);
-			System.out.println();
-		}
+		//preiau in endTime timpul la care s-a terminat programul
 		int endTime = (int) System.nanoTime();
+		//afisez timpul de rulare scazand endTime-startTime
 		System.out.println("Time: " + (endTime - startTime));
 	
 	}
--- L1/src/Word.java
+++ L1/src/Word.java
@@ -2,22 +2,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Word {
-	ArrayList<String> myNeighbours = new ArrayList<String>(Arrays.asList(""));
+	ArrayList<StringBuilder> myNeighbours = new ArrayList<StringBuilder>();
 	private int dim = 0;
 		
-	public void setNeighbourhood(int index, String[] words, boolean[][] matrix, int n)
+	public void setNeighbourhood(int index, StringBuilder[] words, boolean[][] matrix, int n)
 	{
 		for(int i = 0; i < n; i++)
-			if(matrix[index][i] == true)
+			if(matrix[index][i] == true && index != i)
 			{
 				myNeighbours.add(words[i]);
 				dim++;
 			}
 				
 	}
-	public void printNeighbours(int index)
+	public void printNeighbours(int index, StringBuilder[] words)
 	{
-		for(int i = 0; i < dim; i++)
+		System.out.print("Vecinii lui " + words[index] + ": ");
+		for(int i = 0; i < myNeighbours.size(); i++)
 			System.out.print(myNeighbours.get(i) + " ");
 	}
 }
