3741d944ef92fad9314ec62aa3d5b164427407d5
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-03-02 23:20:12 +0200

    problema 'criptarea' adaugata

--- Criptare/src/com/company/Main.java
+++ Criptare/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+import java.lang.reflect.Array;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,33 +11,34 @@ public class Main {
         String s1 = new String();
         String s2 = new String();
         Scanner scan = new Scanner(System.in);
-        boolean[] ap = new boolean[300];
 
         s1 = scan.next();
         s2 = scan.next();
 
         int[] values = new int[300];
+
+        int sum = 0;
         for(int i = 0; i < s2.length(); i++)
         {
             Random rand = new Random();
             values[i] = rand.nextInt(100) + 1;
-            int index = (int)s2.charAt(i);
-            ap[index] = true;
-        }
-
-        int sum = 0;
-        for(int i = 0; i < s1.length(); i++)
-        {
-            int index = (int)s1.charAt(i);
-            if(ap[index] == true)
+            for(int j = 0; j < s1.length(); j++)
             {
-                sum += values[i] + i + 1;
+                if(s2.charAt(i) == s1.charAt(j))
+                    sum += values[i] + j + 1;
             }
         }
+
+
+
         if(sum <= 100)
             System.out.println("CRIPTARE NEVALIDA");
         else
             System.out.println("CRIPTARE VALIDA");
-        System.out.print("S = " + sum);
+
+        System.out.print("Valorile asociate elementelor din sirul B: ");
+        for(int i = 0; i < s2.length(); i++)
+            System.out.print(values[i]+ " ");
+        System.out.print("\nS = " + sum);
     }
 }
