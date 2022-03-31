74a88f66d7b49202d52228ff84b06969ed00ee78
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-03-31 17:38:21 +0300

    Lab6

--- Lab5/src/main/java/Tema/CatalogUtil.java
+++ Lab5/src/main/java/Tema/CatalogUtil.java
@@ -1,5 +1,6 @@
 package Tema;
 
+import Exceptions.InvalidCatalogException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.awt.*;
@@ -7,5 +8,21 @@ import java.io.File;
 import java.io.IOException;
 
 public class CatalogUtil {
+    public void save(Catalog catalog, String path)
+            throws IOException {
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(
+                new File(path),
+                catalog);
+    }
+
+    public Catalog load(String path)
+            throws InvalidCatalogException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Catalog catalog = objectMapper.readValue(
+                new File(path),
+                Catalog.class);
+        return catalog;
+    }
 }
