00d71ec8cb004b126d9c8055ac0468dfc3cbcd0f
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-05-18 21:28:37 +0300

    lab10

---
+++ Lab10/src/main/java/Client.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        String serverAddress = "127.0.0.1"; // The server's IP address
+        int PORT = 8100; // The server's port
+        try (
+                Socket socket = new Socket(serverAddress, PORT);
+                PrintWriter out =
+                        new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader (
+                        new InputStreamReader(socket.getInputStream())) ) {
+            // Send a request to the server
+            String request = "World";
+            out.println(request);
+            // Wait the response from the server ("Hello World!")
+            String response = in.readLine();
+            System.out.println(response);
+        } catch (UnknownHostException e) {
+            System.err.println("No server listening... " + e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
